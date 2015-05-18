0185f48d9357562e7dc56c49a5c78dc7dfd9db19
Kelly Bristol <bristolkr@gmail.com>
2015-05-18 16:50:19 -0400

    aww yiss it works

--- blog.rb
+++ blog.rb
@@ -2,20 +2,6 @@ require 'erb'
 require 'webrick'
 require 'yaml'
 
-# File.open 'index.html', 'w' do |file| 
-#   file.write template.result
-# end
-
-# root = File.expand_path "."
-
-# server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
-
-# server.mount_proc '/' do |req, res|
-#   @post = YAML.load_file('post.yml')
-#   template = ERB.new(File.read('index.html.erb'))
-#   res.body = template.result
-# end
-
 ROOT = File.dirname(__FILE__)
 
 PORT = ENV["PORT"] || 8000
